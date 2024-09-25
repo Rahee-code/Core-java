4ef967d46c678e3e0cc81b761709811605bcb593
Rahee-code <rahikhatu14@gmail.com>
2024-09-25 11:57:53 +0530

    Add files via upload

---
+++ polymorphism/Sentence.java
@@ -0,0 +1,36 @@
+package poly;
+
+class Add
+{
+	void sum(int... a)
+	{
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+			sum+=a[i];
+		}
+		System.out.println(sum);
+	}
+	void printSen(String... words)
+	{
+		String Sentence=" ";
+		for(int i=0;i<words.length;i++)
+		{
+			Sentence+=words[i]+" ";
+		}
+		System.out.println(Sentence);
+	}
+}
+
+public class Sentence {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add ad=new Add();
+		ad.printSen("java","is","a","programming","Language");
+		ad.sum(10,20,30,40);
+
+	}
+
+}
